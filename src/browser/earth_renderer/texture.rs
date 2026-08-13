86828cd6642698e2fcea8353df6d835477a07433
aatuh <aatu.harju@gmail.com>
2026-08-13 22:51:14 +0300

    style: format Earth texture setup

--- src/browser/earth_renderer/texture.rs
+++ src/browser/earth_renderer/texture.rs
@@ -12,7 +12,11 @@ pub(super) fn create(gl: &Gl) -> Result<WebGlTexture, JsValue> {
     gl.pixel_storei(Gl::UNPACK_ALIGNMENT, 1);
     gl.tex_parameteri(Gl::TEXTURE_2D, Gl::TEXTURE_WRAP_S, Gl::REPEAT as i32);
     gl.tex_parameteri(Gl::TEXTURE_2D, Gl::TEXTURE_WRAP_T, Gl::CLAMP_TO_EDGE as i32);
-    gl.tex_parameteri(Gl::TEXTURE_2D, Gl::TEXTURE_MIN_FILTER, Gl::LINEAR_MIPMAP_LINEAR as i32);
+    gl.tex_parameteri(
+        Gl::TEXTURE_2D,
+        Gl::TEXTURE_MIN_FILTER,
+        Gl::LINEAR_MIPMAP_LINEAR as i32,
+    );
     gl.tex_parameteri(Gl::TEXTURE_2D, Gl::TEXTURE_MAG_FILTER, Gl::LINEAR as i32);
     let pixels = surface::pixels_rgba();
     gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
