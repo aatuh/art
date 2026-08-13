14ff5659dbef2e4fe7c891932e4400e10bb9b55e
aatuh <aatu.harju@gmail.com>
2026-08-13 21:21:50 +0300

    refactor: link Earth renderer helper modules

---
+++ src/browser/earth_renderer_parts/modules.rs
@@ -0,0 +1,10 @@
+#[path = "../earth_renderer/canvas.rs"]
+mod canvas;
+#[path = "../earth_renderer/gl_program.rs"]
+mod gl_program;
+#[path = "../earth_renderer/texture.rs"]
+mod texture;
+#[path = "../earth_renderer/triangle.rs"]
+mod triangle;
+#[path = "../earth_surface.rs"]
+mod surface;
