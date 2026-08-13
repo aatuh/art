29ad81d483745411edb3ce31582af95b72817716
aatuh <aatu.harju@gmail.com>
2026-08-13 19:28:28 +0300

    refactor: add a neutral virtual-camera API

---
+++ src/camera_api.rs
@@ -0,0 +1,3 @@
+//! Neutral names for the virtual exhibition camera used by browser adapters.
+
+pub use crate::exhibition_camera::{SpaceflightInput as CameraInput, SpaceflightState as CameraState};
